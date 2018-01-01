3d82afd28e67d21d92817de25e9e9ecbf11485c8
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2017-12-31 23:18:39 -0300

    Added MIT License information

--- vec2.js
+++ vec2.js
@@ -1,4 +1,6 @@
 /*--------------------------------------------------------------------------------*\
+ | MIT License                                                                    |
+ |                                                                                |
  | Copyright (c) 2017 Gustavo Takachi Toyota                                      |
  |                                                                                |
  | Permission is hereby granted, free of charge, to any person obtaining a copy   |
